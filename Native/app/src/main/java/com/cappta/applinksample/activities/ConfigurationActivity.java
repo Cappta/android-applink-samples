f4166b7dbf67c2827798d0b0c2c2f761cf6c0e97
rmterra <r.mirandaterra@gmail.com>
2017-04-20 15:14:09 -0300

    Remove unused imports

--- Native/app/src/main/java/com/cappta/applinksample/activities/ConfigurationActivity.java
+++ Native/app/src/main/java/com/cappta/applinksample/activities/ConfigurationActivity.java
@@ -4,20 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.cappta.applinksample.R;
-import com.cappta.applinksample.model.ApprovedResponse;
-
-import org.json.JSONObject;
 
 public class ConfigurationActivity extends Activity implements View.OnClickListener {
 
