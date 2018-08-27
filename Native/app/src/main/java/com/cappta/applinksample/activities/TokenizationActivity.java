ed7bfa976f5d876e7a240d4fd0bf498a35d2114d
Unknown <lourival.cicero1@gmail.com>
2018-08-27 13:00:44 -0300

    Create tokenzation menu actvity

---
+++ Native/app/src/main/java/com/cappta/applinksample/activities/TokenizationActivity.java
@@ -0,0 +1,32 @@
+package com.cappta.applinksample.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.cappta.applinksample.R;
+
+public class TokenizationActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onClick(View view) {}
+
+    private void startActivity(Class activityClass)
+    {
+        Intent intent = new Intent(this, activityClass);
+
+        startActivity(intent);
+    }
+}
